cc36efff522547a49ac2ff2ee6d8e021f16a9b33
ryanparketh <ryanpark@rocketmail.com>
2022-08-31 21:53:48 -0400

    updated most recent code

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 // Assignment code here
 // create lets for characters that can be used
-let lowerCase =['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Z', 'X', 'Y', 'Z'];
 let special = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-//create limits for length of character 8-128
+let selection = [];
 
+//create prompt for password
+let length = parseInt(
+    prompt('How many characters would you like your password to be?')
+);
 //create input valitation for limits for characters
-if (length > 128){
-    alert('Password length cannot be longer than 128 characters');
+while (length <=8 || length >= 128) {
+    window.alert('Password length cannot be shorter then 8 characters and be longer than 128 characters');
     return null;
 }
 
-if (length < 8){
-    alert('Password length cannot be shorter than 8 characters')
-    return null;
+function writePassword(){
+let lowercaseChoice = window.confirm ('Click OK to include lowercase characters');
+let uppercaseChoice = window.confirm ('Click OK to include uppercase characters');
+let specialChoice = window.confirm('Click OK to include special characters');
+let numberChoice = window.confirm ('Click OK to include number characters');
 }
+
+if (lowercaseChoice){
+    selection.concat(lowerCase)
+}
+if(uppercaseChoice){
+    selection.concat(upperCase)
+}
+if(specialChoice){
+    selection.concat(special)
+}
+if (numberChoice){
+    selection.concat(numbers)
+}
+
+
 
 
 // Get references to the #generate element
@@ -23,10 +44,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
