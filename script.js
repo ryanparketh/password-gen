659cf03f8f7d221cc7c8d3fdc9965d7b85ea5057
ryanparketh <ryanpark@rocketmail.com>
2022-09-01 12:14:01 -0400

    fixed a few errors that occured in console

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 let special = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let selection = [];
+var generateBTn;
 
 
-function writePassword(){
+function generatePassword(){
     let length = parseInt(
         prompt('How many characters would you like your password to be?')
     );
@@ -20,6 +21,7 @@ function writePassword(){
     if(length > 128){
         alert('Password must contain less then 128 characters');
     }
+
 let lowercaseChoice = window.confirm ('Click OK to include lowercase characters');
 let uppercaseChoice = window.confirm ('Click OK to include uppercase characters');
 let specialChoice = window.confirm('Click OK to include special characters');
@@ -38,10 +40,11 @@ if(special){
 if (numbers){
     selection.concat(numbers)
 }
-
-
-
-
+function writePassword(){
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
