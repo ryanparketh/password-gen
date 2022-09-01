46f5715b0525ea1038f8fa088e87b1477ea55a23
ryanparketh <ryanpark@rocketmail.com>
2022-09-01 17:58:11 -0400

    latest code update

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-
+    console.log(password)
     passwordText.value = password;
 
 }
